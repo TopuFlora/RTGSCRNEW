f8c12dcc4503a4c200f0d3e9d4a207a1a2157a0f
TopuFlora <32592220+TopuFlora@users.noreply.github.com>
2021-02-01 14:10:05 +0600

    RTGSCRNEW1

---
+++ LogOut.aspx.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using FloraSoft;
+namespace Check
+{
+    public partial class LogOut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UserDB db = new UserDB();
+            //FormsAuthentication.SignOut();
+            //Response.Redirect("Login.aspx");
+            int userID = int.Parse(this.Context.User.Identity.Name);
+            db.LogOut(userID);
+        }
+
+
+    }
+}
